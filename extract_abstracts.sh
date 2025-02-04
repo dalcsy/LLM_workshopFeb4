ff152c752124cfd1d250a3b8f229ea6cbb0306b8
dalcsy <53729028+dalcsy@users.noreply.github.com>
2025-02-04 11:28:37 +0000

    Cleaned_abstracts

--- extract_abstracts.sh
+++ extract_abstracts.sh
@@ -1,1 +1,2 @@
-grep 
+grep "Abstract" Nature_Genetics_24.txt | sed 's/^Abstract: //' | awk 'NF >= 100'> Nature_genetics_24_abstracts_only.txt
+grep "Abstract" American_Journal_of_Human_Genetics_24.txt | sed 's/^Abstract: //' | awk 'NF >= 100'> American_Journal_of_Human_Genetics_24_only.txt
