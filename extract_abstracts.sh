e976607be6a7299a9f6c98ae85aa2ea76f245847
dalcsy <53729028+dalcsy@users.noreply.github.com>
2025-02-04 11:41:29 +0000

    cleaned_data

--- extract_abstracts.sh
+++ extract_abstracts.sh
@@ -1,2 +1,5 @@
 grep "Abstract" Nature_Genetics_24.txt | sed 's/^Abstract: //' | awk 'NF >= 100'> Nature_genetics_24_abstracts_only.txt
 grep "Abstract" American_Journal_of_Human_Genetics_24.txt | sed 's/^Abstract: //' | awk 'NF >= 100'> American_Journal_of_Human_Genetics_24_only.txt
+grep "Abstract" BMC_genomics_24.txt | sed 's/^Abstract: //' | awk 'NF >= 100'> BMC_genomics_24_only.txt
+grep "Abstract" Genome_Biology_24.txt | sed 's/^Abstract: //' | awk 'NF >= 100'> Genome_Biology_24_only.txt
+grep "Abstract" Human_Molecular_Genetics_24.txt | sed 's/^Abstract: //' | awk 'NF >= 100'> Human_Molecular_Genetics_only.txt
